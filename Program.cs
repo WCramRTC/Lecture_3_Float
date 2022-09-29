87f66b8c766d5bb001fdf35c737f4e2c1173ab52
WCram <wcram@rtc.edu>
2022-09-28 19:13:58 -0700

    Updated with code about float

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// William
+// 09/28/2022
+// Assignment 2
 
 namespace Lecture_3_Float
 {
@@ -10,6 +13,32 @@ namespace Lecture_3_Float
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+
+            // float point numbers
+            // for floating numbers, end with an f
+            float sliceOfPizza = .25f;
+
+            // for decimal, end with an m
+            decimal dollarAmount = 1.52m;
+
+            double coordinateX = 52.5;
+
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            int usersNumber = int.Parse(userInput);
+
+            Console.WriteLine(usersNumber + usersNumber);
+
+            //int userNumber2 = int.Parse(Console.ReadLine());
+
+            // casting
+
+            // try / catch
+
+
+
+        } // Main
+
+    } // class
+
+} // namespace
